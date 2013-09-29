0278e19acd1f43c6a00bcebd8305f56a2e308215
jan.misek <ajan.misek@rclick.cz>
2013-09-29 14:51:16 +0200

    javascript consolidated

---
+++ sources/static/lib/handlebars/lib/handlebars/compiler/base.js
@@ -0,0 +1,21 @@
+var handlebars = require("./parser");
+var Handlebars = require("../base");
+
+// BEGIN(BROWSER)
+Handlebars.Parser = handlebars;
+
+Handlebars.parse = function(input) {
+
+  // Just return if an already-compile AST was passed in.
+  if(input.constructor === Handlebars.AST.ProgramNode) { return input; }
+
+  Handlebars.Parser.yy = Handlebars.AST;
+  return Handlebars.Parser.parse(input);
+};
+
+Handlebars.print = function(ast) {
+  return new Handlebars.PrintVisitor().accept(ast);
+};
+// END(BROWSER)
+
+module.exports = Handlebars;
