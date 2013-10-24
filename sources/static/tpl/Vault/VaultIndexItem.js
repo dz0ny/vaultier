7bb2ec2bff4af03fc6cb7c7801ece7df155eea81
jan.misek <ajan.misek@rclick.cz>
2013-10-24 14:06:01 +0200

    Vault beautification

--- sources/static/tpl/Vault/VaultIndexItem.js
+++ sources/static/tpl/Vault/VaultIndexItem.js
@@ -19,7 +19,7 @@ function program1(depth0,data) {
   }
 function program2(depth0,data) {
   
-  var buffer = '', stack1, hashTypes, hashContexts, options;
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options;
   data.buffer.push("\r\n        <div class=\"vlt-header\">\r\n            <div class=\"vlt-icon\">\r\n                <img src=\"/static/images/icon-vault-dark-blue.png\">\r\n            </div>\r\n            <div class=\"vlt-title\">\r\n                <h3>\r\n                    ");
   hashTypes = {};
   hashContexts = {};
@@ -28,8 +28,8 @@ function program2(depth0,data) {
   data.buffer.push("\r\n                </h3>\r\n\r\n            </div>\r\n        </div>\r\n        <div class=\"vlt-body\">\r\n            ");
   hashTypes = {};
   hashContexts = {};
-  options = {hash:{},contexts:[depth0,depth0],types:["ID","INTEGER"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.ellipsis || depth0.ellipsis),stack1 ? stack1.call(depth0, "description", 250, options) : helperMissing.call(depth0, "ellipsis", "description", 250, options))));
+  stack2 = helpers['if'].call(depth0, "description", {hash:{},inverse:self.program(5, program5, data),fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
   data.buffer.push("\r\n        </div>\r\n        <div class=\"vlt-footer\">\r\n            <div class=\"vlt-footer-item help-block\">\r\n                Updated: ");
   hashTypes = {};
   hashContexts = {};
@@ -41,6 +41,23 @@ function program2(depth0,data) {
   data.buffer.push(escapeExpression(((stack1 = helpers.ucfirst || depth0.ucfirst),stack1 ? stack1.call(depth0, "created_by.nickname", options) : helperMissing.call(depth0, "ucfirst", "created_by.nickname", options))));
   data.buffer.push("\r\n            </div>\r\n            <div class=\"clearfix\"></div>\r\n        </div>\r\n    ");
   return buffer;
+  }
+function program3(depth0,data) {
+  
+  var buffer = '', stack1, hashTypes, hashContexts, options;
+  data.buffer.push("\r\n                ");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},contexts:[depth0,depth0],types:["ID","INTEGER"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  data.buffer.push(escapeExpression(((stack1 = helpers.ellipsis || depth0.ellipsis),stack1 ? stack1.call(depth0, "description", 250, options) : helperMissing.call(depth0, "ellipsis", "description", 250, options))));
+  data.buffer.push("\r\n            ");
+  return buffer;
+  }
+
+function program5(depth0,data) {
+  
+  
+  data.buffer.push("\r\n                No description given\r\n            ");
   }
 
   hashTypes = {};
