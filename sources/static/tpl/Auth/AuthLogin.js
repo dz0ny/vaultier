ca54e6a102e0c67e98cb8e57112314c5e4f4a5f4
Jan Misek <jan.misek@rclick.cz>
2014-01-02 00:11:15 +0100

    search - auth ui tweaks

--- sources/static/tpl/Auth/AuthLogin.js
+++ sources/static/tpl/Auth/AuthLogin.js
@@ -6,50 +6,50 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = '', stack1, stack2, hashTypes, hashContexts, options;
-  data.buffer.push("\r\n\r\n                        <ul class=\"nav nav-tabs vlt-login-tabs\">\r\n                            <li class=\"AuthLoginLatest\">\r\n                                ");
+  data.buffer.push("\r\n\r\n                            <ul class=\"nav nav-tabs vlt-login-tabs\">\r\n                                <li class=\"AuthLoginLatest\">\r\n                                    ");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "AuthLogin.latest", options) : helperMissing.call(depth0, "link-to", "AuthLogin.latest", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
-  data.buffer.push("\r\n                            </li>\r\n                            <li class=\"AuthLoginSwitch\">\r\n                                ");
+  data.buffer.push("\r\n                                </li>\r\n                                <li class=\"AuthLoginSwitch\">\r\n                                    ");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "AuthLogin.switch", options) : helperMissing.call(depth0, "link-to", "AuthLogin.switch", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
-  data.buffer.push("\r\n                            </li>\r\n                        </ul>\r\n\r\n                    ");
+  data.buffer.push("\r\n                                </li>\r\n                            </ul>\r\n\r\n                        ");
   return buffer;
   }
 function program2(depth0,data) {
   
   
-  data.buffer.push("\r\n                                    Latest\r\n                                ");
+  data.buffer.push("\r\n                                        Latest\r\n                                    ");
   }
 
 function program4(depth0,data) {
   
   
-  data.buffer.push("\r\n                                    Switch\r\n                                ");
+  data.buffer.push("\r\n                                        Switch\r\n                                    ");
   }
 
 function program6(depth0,data) {
   
   
-  data.buffer.push("Create new\r\n                    account here");
+  data.buffer.push("Create\r\n                            new\r\n                            account here");
   }
 
-  data.buffer.push("<div class=\"row\">\r\n    <div class=\"col-md-12\">\r\n\r\n        <div class=\"vlt-dialog vlt-dialog-window vlt-login\">\r\n            <div class=\"vlt-dialog-content\">\r\n        <form class=\"form-horizontal\" role=\"form\">\r\n\r\n                <div class=\"vlt-dialog-header\">\r\n                    <h2>Login</h2>\r\n\r\n                    ");
+  data.buffer.push("<div class=\"row\">\r\n    <div class=\"col-md-12\">\r\n\r\n        <div class=\"vlt-dialog vlt-dialog-window vlt-login\">\r\n            <div class=\"vlt-dialog-content\">\r\n                <form class=\"form-horizontal\" role=\"form\">\r\n\r\n                    <div class=\"vlt-dialog-header\">\r\n                        <h2>Login</h2>\r\n\r\n                        ");
   hashTypes = {};
   hashContexts = {};
   stack1 = helpers['if'].call(depth0, "props.latestUser", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\r\n\r\n                </div>\r\n                <div class=\"vlt-dialog-body\">\r\n\r\n                    <div class=\"tab-content\">\r\n                        <div class=\"tab-pane active\">\r\n                            ");
+  data.buffer.push("\r\n\r\n                    </div>\r\n                    <div class=\"vlt-dialog-body\">\r\n\r\n                        <div class=\"tab-content\">\r\n                            <div class=\"tab-pane active\">\r\n                                ");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   data.buffer.push(escapeExpression(((stack1 = helpers.outlet || depth0.outlet),stack1 ? stack1.call(depth0, "AuthLogin", options) : helperMissing.call(depth0, "outlet", "AuthLogin", options))));
-  data.buffer.push("\r\n                        </div>\r\n                    </div>\r\n\r\n\r\n                </div>\r\n                <div class=\"vlt-dialog-footer\">\r\n                    Do not have account yet? ");
+  data.buffer.push("\r\n                            </div>\r\n                        </div>\r\n\r\n\r\n                    </div>\r\n                    <div class=\"vlt-dialog-footer\">\r\n                        <div class=\"pull-left\">\r\n                            Do not have account yet? ");
   hashContexts = {'class': depth0};
   hashTypes = {'class': "STRING"};
   options = {hash:{
@@ -57,7 +57,7 @@ function program6(depth0,data) {
   },inverse:self.noop,fn:self.program(6, program6, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "AuthRegister", options) : helperMissing.call(depth0, "link-to", "AuthRegister", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
-  data.buffer.push("\r\n                    <button type=\"submit\" ");
+  data.buffer.push("\r\n                        </div>\r\n                        <button type=\"submit\" ");
   hashTypes = {};
   hashContexts = {};
   data.buffer.push(escapeExpression(helpers.action.call(depth0, "login", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
@@ -68,7 +68,7 @@ function program6(depth0,data) {
     'disabled': ("loginButtonDisabled")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   data.buffer.push(escapeExpression(((stack1 = helpers['bind-attr'] || depth0['bind-attr']),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "bind-attr", options))));
-  data.buffer.push(" class=\"btn btn-primary\">\r\n                        Login\r\n                    </button>\r\n                </div>\r\n                </form>\r\n            </div>\r\n        </div>\r\n    </div>\r\n</div>\r\n\r\n\r\n\r\n");
+  data.buffer.push("\r\n                                class=\"btn btn-primary\">\r\n                            Login\r\n                        </button>\r\n                    </div>\r\n                </form>\r\n            </div>\r\n        </div>\r\n    </div>\r\n</div>\r\n\r\n\r\n\r\n");
   return buffer;
   
 });
