573eee8e2c6bdc8c0efc0f8a8095c2127beebb91
Jan Misek <jan.misek@rclick.cz>
2014-01-02 11:51:14 +0100

    search - django_compressor removed

--- sources/app/settings_base.py
+++ sources/app/settings_base.py
@@ -158,11 +158,9 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'vaultier',
-    'compressor',
     'south',
     'rest_framework',
     'django_extensions',
-    # 'django_handlebars',
 )
 
 # A sample logging configuration. The only tangible logging
