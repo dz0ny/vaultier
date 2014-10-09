5714be1efb324730e7af220be4c0290481323734
Jakub Randak <jakub.randak@rclick.cz>
2014-10-09 12:44:29 +0200

    Move config.js view to standard API view #218

--- vaultier/vaultier/settings/base.py
+++ vaultier/vaultier/settings/base.py
@@ -116,7 +116,8 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'vaultier/business'),
     os.path.join(PROJECT_PATH, 'vaultier'),
 
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Put strings here, like "/home/html/django_templates" or
+    # "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
--- vaultier/vaultier/views.py
+++ vaultier/vaultier/views.py
@@ -4,6 +4,7 @@ from django.template.context import Context
 from django.conf import settings
 import json
 import pkg_resources
+from rest_framework.views import APIView
 
 
 def index(request):
@@ -14,22 +15,34 @@ def index(request):
     }))
 
 
-def config(request):
-    script = json.dumps({
-        'FT_FEATURES': settings.FT_FEATURES,
-        'VERSION': pkg_resources.get_distribution("Vaultier").version,
-        'invitation_lifetime': settings.VAULTIER.get('invitation_lifetime'),
-        'registration_allow': settings.VAULTIER.get('registration_allow')
-    })
-    script = 'InitializeConfig = function(app) {  app.Config = Ember.Object.extend('+script+'); }'
+class ConfigView(APIView):
+    """
+    View to provide JS configuration
+    """
+    def get(self, request):
+        """
+        Get configuration from settings, format it and return
+        """
+        # get settings and transform it to json
+        conf_settings = json.dumps({
+            'FT_FEATURES': settings.FT_FEATURES,
+            'VERSION': pkg_resources.get_distribution("Vaultier").version,
+            'invitation_lifetime': settings.VAULTIER.get(
+                'invitation_lifetime'),
+            'registration_allow': settings.VAULTIER.get('registration_allow')
+        })
+        # add settings to script
+        script = 'InitializeConfig = function(app) { ' \
+                 'app.Config = Ember.Object.extend(%s); }' % conf_settings
 
-    return HttpResponse(script, mimetype='text/javascript')
+        return HttpResponse(script, content_type='text/javascript')
 
 
 def error404(request):
     return redirect('/#'+request.path)
 
-#def dev_mail(request):
-#    context = build_context(Member.objects.filter(status=MemberStatusField.STATUS_INVITED).reverse()[0])
+# def dev_mail(request):
+#    context = build_context(Member.objects.filter(
+#        status=MemberStatusField.STATUS_INVITED).reverse()[0])
 #    plain, html = render_email('mailer/invitation', context)
 #    return HttpResponse(html)
