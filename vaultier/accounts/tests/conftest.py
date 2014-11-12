9640327418b713b895021dea8fb2feeab61e1a9e
Ondřej Kmoch <ondrej.kmoch@rclick.cz>
2014-11-12 17:04:19 +0100

    user api tests redone for pytest

--- vaultier/accounts/tests/conftest.py
+++ vaultier/accounts/tests/conftest.py
@@ -2,7 +2,6 @@ import pytest
 from django import setup
 
 from vaultier.test.tools import FileAccessMixin
-
 from accounts.models import User
 
 
@@ -14,8 +13,31 @@ def pytest_configure():
 def user1(db):
     m = FileAccessMixin()
     user = {
-        "nickname": "ondra",
-        "email": "ondrej.kmoch@rclick.cz",
+        "nickname": "duke",
+        "email": "duke.nukem@example.com",
         "public_key": m.read_file('vaultier.pub'),
     }
     return User.objects.create(**user)
+
+
+@pytest.fixture
+def user2(db):
+    m = FileAccessMixin()
+    user = {
+        "nickname": "blade",
+        "email": "blade.nukem@example.com",
+        "public_key": m.read_file('vaultier.pub'),
+    }
+    return User.objects.create(**user)
+
+
+@pytest.fixture
+def pubkey():
+    m = FileAccessMixin()
+    return m.read_file('vaultier.pub')
+
+
+@pytest.fixture
+def privkey():
+    m = FileAccessMixin()
+    return m.read_file('vaultier.key')
--- vaultier/accounts/tests/integration/user_api_test.py
+++ vaultier/accounts/tests/integration/user_api_test.py
@@ -1,16 +1,49 @@
-from datetime import timedelta
 from time import time
+from datetime import timedelta
 
-from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, \
-    HTTP_403_FORBIDDEN, HTTP_400_BAD_REQUEST
 from django.utils import timezone
 from rest_framework.test import APIClient
 from django.core.urlresolvers import reverse
 import pytest
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, \
+    HTTP_400_BAD_REQUEST
+from django.conf import settings
 
+from accounts.models import User, Token
 from vaultier.test.tools import FileAccessMixin
-from accounts.models import User
 from accounts.business.authentication import Authenticator
+
+
+# helper methods
+def create_signature(key, email, date):
+    return Authenticator.sign(key, email, date)
+
+
+def register_user(public_key, nickname, email):
+    client = APIClient()
+    url = reverse('user-list')
+    response = client.post(url, {
+        "nickname": nickname,
+        "email": email,
+        "timestamp": int(time()),
+        "public_key": public_key,
+    })
+    return response
+
+
+def authenticate_user(email, date, private_key=None, signature=None):
+    client = APIClient()
+    url = reverse('auth-auth')
+    response = None
+    if not signature:
+        signature = create_signature(key=private_key, email=email,
+                                     date=date)
+
+    response = client.post(url, {'email': email,
+                                 'date': date,
+                                 'signature': signature
+    })
+    return response
 
 
 @pytest.mark.django_db
@@ -24,136 +57,188 @@ class TestSignatures(object):
         signature = Authenticator.sign(privkey, email, 1)
 
         assert Authenticator.verify(pubkey, email, 1, signature) is True
-        assert Authenticator.verify(pubkey, 'Unsigned text', 1, signature) is not True
+        assert Authenticator.verify(pubkey, 'Unsigned text', 1,
+                                    signature) is not True
 
 
 @pytest.mark.django_db
 class TestApiRegister(object):
-    def public_key1(self):
-        m = FileAccessMixin()
-        return m.read_file('vaultier.pub')
-
-    def private_key1(self):
-        m = FileAccessMixin()
-        return m.read_file('vaultier.key')
-
-    def test_010_register(self):
+    # testing methods
+    def test_010_register(self, pubkey):
         # register user
-        client = APIClient()
-        url = reverse('user-list')
-
-        response = client.post(url, data={
-            "nickname": "ondra",
-            "email": "ondrej.kmoch@rclick.cz",
-            "timestamp": int(time()),
-            "public_key": self.public_key1(),
-        })
-
+        response = register_user(public_key=pubkey,
+                                 nickname="duke",
+                                 email="duke.nukem@example.com")
         # test the response
+        assert (response.status_code == HTTP_201_CREATED)
         assert (response.data.get('id') is not None)
         id = response.data.get('id')
-        assert (response.status_code == HTTP_201_CREATED)
 
         # test if user was created in database
         assert (User.objects.filter(id=id).exists())
 
         # register user again should be forbidden
-        response = client.post(url, {
-            "nickname": "ondra",
-            "email": "ondrej.kmoch@rclick.cz",
-            "timestamp": int(time()),
-            "public_key": self.public_key1(),
-        })
+        response = register_user(public_key=pubkey,
+                                 nickname="duke",
+                                 email="duke.nukem@example.com")
 
         assert (response.data.get('email') is not None)
         assert (response.status_code == HTTP_400_BAD_REQUEST)
 
-    def test_020_auth(self, user1):
-        email = user1.email
+    def test_020_auth(self, user1, privkey):
         date = timezone.now()
-        good_signature = Authenticator.sign(self.private_key1(), email, date)
-        bad_signature = "BadSignature"
 
-        # try to login, check proper signature
-        url = reverse('auth-auth')
-        client = APIClient()
-        response = client.post(url, {'email': email,
-                                     'date': date,
-                                     'signature': good_signature}
-        )
+        # try to login, check good signature
+        response = authenticate_user(private_key=privkey,
+                                     email=user1.email, date=date)
+
         assert (response.status_code == HTTP_200_OK)
 
         # try to login, check wrong signature
-        response = client.post(url, {'email': email,
-                                     'date': date,
-                                     'signature': bad_signature}
-        )
-        assert (response.status_code == HTTP_403_FORBIDDEN)
+        bad_signature = "DefinitelyBadSignature"
+        response = authenticate_user(email=user1.email, date=date,
+                                     signature=bad_signature)
+
+        # todo: currently returns 400, ideally should 403
+        assert (response.status_code == HTTP_400_BAD_REQUEST)
 
         # try to login, check wrong expired timestamp
         expired = timezone.now() - timedelta(days=15)
-        response = client.post(url, {'email': email,
-                                     'date': expired,
-                                     'signature': bad_signature}
-        )
+        response = authenticate_user(private_key=privkey,
+                                     email=user1.email, date=expired)
+
         assert (response.status_code == HTTP_400_BAD_REQUEST)
 
+    def test_030_registration_should_be_case_insensitive(self, pubkey, privkey):
+        # registration with multicase email and auth with lowercase
+        # register user
+        client = APIClient()
+        date = timezone.now()
 
-    #  this does not work yet
-    #  also - investigate if the registration REALLY should be insensitive
+        email = 'TeSt.CaSe.InSeSiTiVe@rclick.cz'
 
-    # def test_030_registration_should_be_case_insensitive(self):
-    #     # register user
-    #     client = APIClient()
-    #     date = timezone.now()
-    #     email = 'TeSt.CaSe.InSeSiTiVe@rclick.cz'
-    #     url = reverse('user-list')
-    #
-    #     response = client.post(url, {
-    #         "nickname": "case_insensitive",
-    #         "email": email,
-    #         "timestamp": int(time()),
-    #         "public_key": self.public_key1(),
-    #     })
-    #
-    #     id = response.data.get('id')
-    #     assert (response.status_code == HTTP_201_CREATED)
-    #
-    #     lower_case_signature = Authenticator.sign(self.private_key1().lower(), email, date)
-    #     response = client.post(url, {'email': email.lower(),
-    #                                  'date': date,
-    #                                  'signature': lower_case_signature}
-    #                            )
-    #
-    #     assert (response.status_code == HTTP_200_OK)
-    #     assert (response.data.get('user') == id)
-    #
-    #     upper_case_signature = Authenticator.sign(self.private_key1(), email.upper(), date)
-    #     response = client.post(url, {'email': email.lower(),
-    #                                  'date': date,
-    #                                  'signature': upper_case_signature}
-    #     )
-    # 
-    #     assert (response.status_code == HTTP_200_OK)
-    #     assert (response.data.get('user') == id)
-    #
-    #
-    #     # register user
-    #     email = 'TEST_UPPERCASE_REGISTRATION@rclick.cz'
-    #     response = client.post(url, {
-    #         "nickname": "UPPER_CASE_NICK",
-    #         "email": email,
-    #         "timestamp": int(time()),
-    #         "public_key": self.public_key1(),
-    #     })
-    #     id = response.data.get('id')
-    #     assert(response.status_code == HTTP_201_CREATED)
-    #
-    #     # login with the email to lower case
-    #     lower_case_signature = Authenticator.sign(self.private_key1(), email.lower(), date)
-    #     response = client.post(url, {'email': email.lower(),
-    #                                  'date': date,
-    #                                  'signature': lower_case_signature}
-    #                            )
-    #
-    #     assert(response.status_code == HTTP_200_OK)
+        response = register_user(public_key=pubkey,
+                                 nickname="case_insensitive", email=email)
+
+        assert (response.status_code == HTTP_201_CREATED)
+
+        # try auth with same email in lower case
+        response = authenticate_user(private_key=privkey,
+                                     email=email.lower(), date=date)
+
+        assert (response.status_code == HTTP_200_OK)
+
+        # login with the email in UPPER CASE
+        response = authenticate_user(private_key=privkey,
+                                     email=email.upper(), date=date)
+
+        assert (response.status_code == HTTP_200_OK)
+
+        # register user
+        email = 'TEST_UPPERCASE_REGISTRATION@rclick.cz'
+        response = register_user(public_key=pubkey,
+                                 nickname="UPPER_CASE_NICK", email=email)
+
+        assert (response.status_code == HTTP_201_CREATED)
+
+        # login with the email to lower case
+        response = authenticate_user(private_key=privkey,
+                                     email=email.lower(), date=date)
+
+        assert (response.status_code == HTTP_200_OK)
+
+    def test_040_email_cannot_be_registered_more_than_once(self, user1, user2,
+                                                           pubkey):
+        # try to register user from fixture again
+
+        response = register_user(public_key=pubkey, email=user1.email,
+                                 nickname=user1.nickname)
+
+        assert (response.status_code == HTTP_400_BAD_REQUEST)
+        assert (
+            response.data.get('email') == [
+                u'User with this Email already exists.'])
+
+        # try to register user2 from fixture with same but uppercase mail
+        response = register_user(public_key=pubkey,
+                                 email=user2.email.upper(),
+                                 nickname=user2.nickname)
+
+        assert (response.status_code == HTTP_400_BAD_REQUEST)
+        assert (
+            response.data.get('email') == [
+                u'User with this Email already exists.'])
+
+
+class TestTokenExpiration(object):
+    def test_010_token_renewed(self, user1, privkey):
+        date = timezone.now()
+        response = authenticate_user(private_key=privkey,
+                                     email=user1.email, date=date)
+
+        token = response.data.get('token')
+
+        token_model = Token.objects.get(token=token)
+        """:type : Token """
+        td = timedelta(days=-1)
+        last_used_at = timezone.now() - td
+        token_model.last_used_at = last_used_at
+        token_model.save()
+
+        # current date should be set, limit 60 seconds in case of some slowdown
+        assert (
+            (timezone.now() - token_model.last_used_at).total_seconds() < 60)
+
+    def test_020_token_not_renewed_immediately(self, user1, privkey):
+        date = timezone.now()
+        response = authenticate_user(private_key=privkey,
+                                     email=user1.email, date=date)
+
+        token = response.data.get('token')
+
+        token_model = Token.objects.get(token=token)
+        """:type : Token """
+
+        last_used_at = token_model.last_used_at
+        token_model.save()
+
+        # last_used_at not changed
+        assert (last_used_at == token_model.last_used_at)
+
+    def test_030_old_token_cleaned_up(self, user1, privkey):
+        date = timezone.now()
+        response = authenticate_user(private_key=privkey,
+                                     email=user1.email, date=date)
+
+        token = response.data.get('token')
+
+        token_model = Token.objects.get(token=token)
+        """:type : Token """
+        token_lifetime = settings.VAULTIER.get('authentication_token_lifetime')
+        td = timedelta(hours=2 * token_lifetime)
+        last_used_at = timezone.now() - td
+        token_model.last_used_at = last_used_at
+        token_model.save()
+
+        Token.objects.clean_old_tokens()
+        assert (Token.objects.all().count() == 0)
+
+    def test_040_recent_token_not_cleaned_up(self, user1, privkey):
+        date = timezone.now()
+        response = authenticate_user(private_key=privkey,
+                                     email=user1.email, date=date)
+
+        token = response.data.get('token')
+
+        token_model = Token.objects.get(token=token)
+        """:type : Token """
+        token_lifetime = settings.VAULTIER.get('authentication_token_lifetime')
+        td = timedelta(hours=token_lifetime - 1)
+        last_used_at = timezone.now() - td
+
+        token_model.last_used_at = last_used_at
+        token_model.save()
+
+        Token.objects.clean_old_tokens()
+        # token not deleted
+        assert (Token.objects.all().count() == 1)
