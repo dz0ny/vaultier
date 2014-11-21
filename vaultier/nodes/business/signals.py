36b25e0e4b1610617bf7d16d3670b00098e4ae16
Ondřej Kmoch <ondrej.kmoch@rclick.cz>
2014-11-21 20:48:48 +0100

    works on targeted websocket, incomplete

--- vaultier/nodes/business/signals.py
+++ vaultier/nodes/business/signals.py
@@ -1,8 +1,20 @@
 import redis
+
 from django.conf import settings
+
+from accounts.models import User
 
 
 def trigger_node_signal(sender, instance, **kwargs):
     r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
                           db=settings.REDIS_DB)
-    r.publish("redischannel", instance.pk)
+
+    # todo: get only users in related to Node
+    users = User.objects.all()
+    clients = []
+    for u in users:
+        clients.append(u.pk)
+        # created: True if created, False if modified
+    message = {'clients': 'clients', 'subject': {'type': kwargs['created'],
+                                               'node': instance.pk}}
+    r.publish("redischannel", message)
--- vaultier/vaultier/websocket/websocket.py
+++ vaultier/vaultier/websocket/websocket.py
@@ -1,10 +1,13 @@
-from gevent import pywsgi
+from gevent import pywsgi, sleep, spawn
 from django import setup
 from rest_framework.exceptions import AuthenticationFailed
 from geventwebsocket.handler import WebSocketHandler
 from geventwebsocket.exceptions import WebSocketError
-
+import json
 from accounts.business.authentication import TokenAuthentication
+import redis
+import re
+from django.conf import settings
 
 
 class ConnectedClient(object):
@@ -58,6 +61,7 @@ class WSApplication(object):
                 while True:
                     try:
                         message = ws.receive()
+                        print message
                     except WebSocketError:
                         curr_user.del_connection(ws)
                         if len(curr_user.connection_list) == 0:
@@ -70,9 +74,45 @@ class WSApplication(object):
                 ws.close()
 
 
+def test(app, p):
+    while True:
+        msg = p.get_message()
+        if msg:
+            print msg
+            try:
+                data = msg['data']
+
+
+                # subjects = data['subject']
+                # subjects = msg['data']['subject']
+                # str = eval(s)
+                # str = json.loads(re.sub(r",\s*(\w+)", r", '\1'", re.sub(r"\{(\w+)", r"{'\1'", s)).replace("'", '"'))
+
+                #todo: find out how to get the stringified non-dictionary something that redis send here into dictionary again
+
+
+                for x, client in app.client_pool.items():
+                    print x
+                    try:
+                        # todo: when we have usefull list of targets iterate over what would be clients from, if match, send msg
+                        client.send(msg['data'])
+                    except WebSocketError:
+                        client.close()
+                        app.del_connection(x)
+            except TypeError:
+                print "err"
+        sleep(5)
+
+
 def main():
     setup()
+    r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
+                          db=settings.REDIS_DB)
+    p = r.pubsub()
+    p.psubscribe("redischannel")
+
     ws_application = WSApplication()
     server = pywsgi.WSGIServer(("", 8000), ws_application.connection_handler,
                                handler_class=WebSocketHandler)
+    spawn(test, ws_application, p)
     server.serve_forever()
