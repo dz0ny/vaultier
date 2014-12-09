aaca732cfd549ccac164a1e33aef883f68f1c134
Vojtech Knaisl <vknaisl@gmail.com>
2014-12-09 12:45:36 +0100

    remove unused method in nodeadapter

--- vaultier/vaultier/scripts/app/dal/adapter/NodeAdapter.js
+++ vaultier/vaultier/scripts/app/dal/adapter/NodeAdapter.js
@@ -7,31 +7,5 @@ ApplicationKernel.namespace('Vaultier.dal.adapter');
  */
 Vaultier.dal.adapter.NodeAdapter = Vaultier.dal.core.RESTAdapter.extend({
 
-    findParents: function (id) {
-        return Utils.RSVPAjax({
-            url: '/api/nodes/' + id + '/path',
-            type: 'get'
-        }).then(function (parentObjects) {
-            parentModels = [];
-            parentObjects.forEach(function (parentObject) {
-                var parentModel = Vaultier.dal.model.Node.create({
-                    id: parentObject.id,
-                    name: parentObject.name,
-                    meta: parentObject.meta,
-                    node_subtype: parentObject.node_subtype,
-                    data: parentObject.data,
-                    color: parentObject.color,
-                    enc_version: parentObject.enc_version,
-                    created_by: parentObject.created_by,
-                    created_at: parentObject.created_at,
-                    updated_at: parentObject.updated_at,
-                    parent: parentObject.parent
-                });
-                parentModels.pushObject(parentModel);
-                console.log(parentModel);
-            });
-            return parentModels;
-        });
-    }
 
 })
