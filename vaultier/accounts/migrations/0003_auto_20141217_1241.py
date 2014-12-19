a392eb400df9ca1ae669a288a062a1b401d33262
Jakub Bokoc <jakub.bokoc@rclick.cz>
2014-12-19 10:50:52 +0100

    removed TYPE contant on node model

---
+++ vaultier/accounts/migrations/0003_auto_20141217_1241.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('nodes', '0007_auto_20141217_1241'),
+        ('accounts', '0002_auto_20141024_0838'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='member',
+            name='node',
+            field=models.ForeignKey(default=None, to='nodes.Node', null=True),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='member',
+            name='user',
+            field=models.ForeignKey(related_name='membership', default=None, to='accounts.User', null=True),
+            preserve_default=True,
+        ),
+    ]
---
+++ vaultier/accounts/migrations/0004_auto_20141217_1338.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0003_auto_20141217_1241'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='member',
+            name='workspace',
+        ),
+        migrations.AlterField(
+            model_name='member',
+            name='node',
+            field=models.ForeignKey(to='nodes.Node', null=True, default=None),
+            preserve_default=True,
+        ),
+    ]
