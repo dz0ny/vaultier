47ca811cc50dcff5d14fb3b7a6850b47ee1b078f
Vojtech Knaisl <vknaisl@gmail.com>
2015-01-27 16:24:14 +0100

    #395 Method for generating random password should be moven to other place

--- vaultier/vaultier/scripts/app/dal/mixin/EncryptedModelMixin.js
+++ vaultier/vaultier/scripts/app/dal/mixin/EncryptedModelMixin.js
@@ -202,34 +202,6 @@ Vaultier.dal.mixin.EncryptedModel.Mixin = Ember.Mixin.create({
 })
 
 /**
- * @todo: should be moved to utils
- * @method generatePassword
- * @returns {string}
- */
-Vaultier.dal.mixin.EncryptedModel.generatePassword = function () {
-    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    var string_length = 16;
-    var randomstring = '';
-    var charCount = 0;
-    var numCount = 0;
-
-    for (var i = 0; i < string_length; i++) {
-        // If random bit is 0, there are less than 3 digits already saved, and there are not already 5 characters saved, generate a numeric value.
-        if ((Math.floor(Math.random() * 2) == 0) && numCount < 3 || charCount >= 5) {
-            var rnum = Math.floor(Math.random() * 10);
-            randomstring += rnum;
-            numCount += 1;
-        } else {
-            // If any of the above criteria fail, go ahead and generate an alpha character from the chars string
-            var rnum2 = Math.floor(Math.random() * chars.length);
-            randomstring += chars.substring(rnum2, rnum2 + 1);
-            charCount += 1;
-        }
-    }
-    return randomstring;
-}
-
-/**
  * Each field to be automatically decrypted/encrypted should be marked by this model attribute decorator
  * @method decryptedField
  * @param encryptedField {String} source encrypted field out of which decrypted field will be decrypted
